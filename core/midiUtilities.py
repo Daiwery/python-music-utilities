6aee6f4280f17db8d9c5418247844f3d2c23c02a
Daiwery <andrey.luk10@gmail.com>
2022-08-05 13:47:44 +0300

    Midi utilities

---
+++ core/midiUtilities.py
@@ -0,0 +1,126 @@
+"""
+Module for working with midi-messages and files.
+"""
+import os.path
+from typing import List, Union
+
+import mido
+
+
+class MidiMessage:
+    """
+    Represents a midi-message.
+    """
+
+    def __init__(self, kind: str, time: float, **kwargs):
+        self.kind = kind
+        # Time can to have different dimension and essence depending on context.
+        self.time = time
+        # Magic hack for mido.Message arguments.
+        self.kwargs = kwargs
+
+    def toMido(self) -> mido.Message:
+        """
+        Transforms himself in midi-message.
+
+        :return: mido.Message corresponding to this message
+        """
+        return mido.Message(type=self.kind, time=self.time, **self.kwargs)
+
+
+class MidiTrack:
+    """
+    Represents a one midi-track.
+    """
+
+    def __init__(self, label: str, instrument: int, messages: List[MidiMessage] = None):
+        """
+        :param label: Name of track.
+        :param instrument: Instrument that is played in this track.
+        :param messages: List of midi-messages.
+        """
+        if messages is None:
+            messages = []
+        self.messages = messages
+        self.label = label
+        self.instrument = instrument
+
+    def append(self, messages: List[MidiMessage]):
+        """
+        Appends list of midi-messages in track.
+
+        :param messages: List of midi-messages.
+        """
+        self.messages += messages
+
+    def toMido(self):
+        """
+        Transforms himself in mido.MidiTrack.
+
+        :return: mido.MidiTrack corresponding to this track.
+        """
+        track = mido.MidiTrack()
+        track.append(mido.MetaMessage(mido.MetaMessage('track_name', name=self.label)))
+        track.append(mido.Message(type="program_change", program=self.instrument, time=0))
+        for message in self.messages:
+            track.append(message.toMido())
+        return track
+
+
+def transform_time(messages: List[MidiMessage], bmp: Union[int, float],
+                   ticks_per_beat: int = 480, to_tick: bool = True) -> List[MidiMessage]:
+    """
+    Transforms raw midi-messages to midi-message with the necessary time’s attributes.
+
+    :param messages: List of raw midi-messages.
+    :param bmp: Number of metronome beats per minute (= number of quarter notes per minute).
+    :param ticks_per_beat: Number of ticks per beat. Specifies the degree of sampling.
+    :param to_tick: Convert to ticks or seconds.
+    :return: List of midi-message with the necessary time’s attributes.
+    """
+    # Sort by time.
+    messages.sort(key=lambda x: x.time)
+
+    # Convert from absolute time to relative time.
+    global_time = 0
+    for i, message in enumerate(messages):
+        time = message.time - global_time
+        global_time = message.time
+
+        messages[i].time = time
+
+    for i, message in enumerate(messages):
+        # Transform to seconds.
+        messages[i].time /= bmp / (4 * 60)
+        if to_tick:
+            # Transform to ticks.
+            messages[i].time = int(
+                mido.second2tick(messages[i].time, ticks_per_beat=ticks_per_beat, tempo=1 / (bmp / 60) * 10 ** 6))
+
+    return messages
+
+
+def _write_to_file(tracks: List[MidiTrack], path: str):
+    """
+    Helper for 'write_to_file' function.
+    """
+    for track in tracks:
+        midi = mido.MidiFile()
+        midi.tracks.append(track.toMido())
+        midi.save(os.path.join(path, track.label + ".midi"))
+
+
+def write_to_file(tracks: List[MidiTrack], path: str, bmp: int,
+                  ticks_per_beat: int = 480):
+    """
+    Writes each track to a separate midi-file.
+    Performs necessary transformations before recording.
+
+    :param tracks: List of midi-tracks.
+    :param path: The directory for files.
+    :param bmp: Number of metronome beats per minute (= number of quarter notes per minute).
+    :param ticks_per_beat: Number of ticks per beat. Specifies the degree of sampling.
+    """
+    for i, track in enumerate(tracks):
+        tracks[i].messages = transform_time(track.messages, bmp=bmp, ticks_per_beat=ticks_per_beat)
+    _write_to_file(tracks, path)
